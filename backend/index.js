46806c69fe93e29b1649025f59755dfc7bfd7a69
SarthakBhoj <bsarthak4u@gmail.com>
2026-04-13 15:40:35 +0530

    mongo

--- backend/index.js
+++ backend/index.js
@@ -8,35 +8,35 @@ app.use(express.json());
 app.use(cors());
 
 // 👉 USE ATLAS URL for deployment
-mongoose.connect("YOUR_MONGODB_ATLAS_URL")
-.then(()=>console.log("DB Connected"))
-.catch(err=>console.log(err));
+mongoose.connect("mongodb+srv://bsarthak4u:Sarthak@3035@sarthak.bof6i30.mongodb.net/?appName=Sarthak")
+    .then(() => console.log("DB Connected"))
+    .catch(err => console.log(err));
 
 const Todo = require("./models/todo");
 
 // ADD
-app.post("/add", async (req,res)=>{
-  const todo = new Todo({ text: req.body.text });
-  await todo.save();
-  res.send(todo);
+app.post("/add", async (req, res) => {
+    const todo = new Todo({ text: req.body.text });
+    await todo.save();
+    res.send(todo);
 });
 
 // GET
-app.get("/get", async (req,res)=>{
-  const data = await Todo.find();
-  res.json(data);
+app.get("/get", async (req, res) => {
+    const data = await Todo.find();
+    res.json(data);
 });
 
 // UPDATE
-app.put("/update/:id", async (req,res)=>{
-  await Todo.findByIdAndUpdate(req.params.id, { text: req.body.text });
-  res.send("Updated");
+app.put("/update/:id", async (req, res) => {
+    await Todo.findByIdAndUpdate(req.params.id, { text: req.body.text });
+    res.send("Updated");
 });
 
 // DELETE
-app.delete("/delete/:id", async (req,res)=>{
-  await Todo.findByIdAndDelete(req.params.id);
-  res.send("Deleted");
+app.delete("/delete/:id", async (req, res) => {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.send("Deleted");
 });
 
-app.listen(5000, ()=>console.log("Server running"));
+app.listen(5000, () => console.log("Server running"));
